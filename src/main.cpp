9783e938d2dfdde6972f0448ee8eb7bc43a6877b
Fahad704 <shaikh.fahad.77377@gmail.com>
2026-06-18 01:51:37 +0530

    Basic diffuse lighting

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,13 @@
 #include "Vector2.h"
 #include <FSWindow.h>
 #include <algorithm>
+#include <climits>
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
+#include <iostream>
 #include <vector>
-#include <climits>
+
 /*
     TODO:
     -Anti aliasing by offsetting rays
@@ -17,10 +20,11 @@
 */
 using Vector = FS::Vector;
 using Vector2 = FS::Vector2;
-FS::Colour backgroundColor = { 64, 64, 64 };
+using Colour = FS::Colour;
+Colour backgroundColor = { 64, 64, 64 };
 
 struct Material {
-    FS::Colour color;
+    Colour color;
     float reflectiveness;
 };
 
@@ -66,6 +70,7 @@ void clearScreen(uint32_t color, FS::RenderState &renderState) {
 Vector normalize(const Vector &vec) {
     return vec / length(vec);
 }
+// vv You're not supposed to use this for path tracing , its already done when tracing paths
 float rayTracedLight(const Vector &point, const Vector &normal, const std::vector<Light> &lights) {
     float intensity = 0.f;
     for (const Light &light : lights) {
@@ -121,32 +126,74 @@ IntersectionData closestIntersection(const Vector &origin, const Vector &directi
 
     return intData;
 }
-Vector reflectRay(const Vector& R,const Vector& N){
-    return (2 * (N * dot(R,N)) - R);
+Vector reflectRay(const Vector &R, const Vector &N) {
+    return (2 * (N * dot(R, N)) - R);
 }
-FS::Colour traceRay(const Vector &origin, const Vector &direction, const int bounceCount,const Scene &scene) {
+// get random number between [0.f,1.f]
+float unitRandom() {
+    float randnum = (rand() / (RAND_MAX + 1.f));
+    return randnum;
+}
+Vector hemisphereRandom(const Vector& normal){
+    Vector randvec = {};
+    //Very inefficient , fix later
+    while(true){
+        randvec = {unitRandom(),unitRandom(),unitRandom()};
+        if(length(randvec) < 1){
+            break;
+        }
+    }
+    normalize(randvec);
+    if(dot(randvec,normal) > 0.f){
+        return randvec;
+    }else{
+        return -randvec;
+    }
+}
+Colour traceRay(const Vector &origin, const Vector &direction, const int bounceCount, const Scene &scene) {
     IntersectionData intersectData = closestIntersection(origin, direction, scene);
     float reflectiveness = intersectData.material.reflectiveness;
     if (intersectData.intersection == INT_MAX) {
         return backgroundColor;
     }
-    FS::Colour color = intersectData.material.color * rayTracedLight(intersectData.point, intersectData.normal, scene.lights);
-    if(bounceCount == 0 || reflectiveness <= 0.f)return color;
-    Vector newDirection = reflectRay(-direction, intersectData.normal);
-    return ((color * (1 - reflectiveness)) + (traceRay(intersectData.point,newDirection, bounceCount-1, scene) * (1-reflectiveness)));
+    Colour color = intersectData.material.color;
+    if (bounceCount == 0 || reflectiveness <= 0.f) {
+        return color;
+    }
+    Vector newDirection = hemisphereRandom(intersectData.normal);//reflectRay(-direction, intersectData.normal);
+    return traceRay(intersectData.point , newDirection , bounceCount-1 , scene) * 0.5f;
+}
+Vector canvasToViewport(float x, float y, FS::RenderState &renderState) {
+    constexpr float d = 1.f;
+    const Vector viewport{ renderState.width / float(renderState.height), 1 };
+    return { x * (viewport.x / float(renderState.width)), y * (viewport.y / float(renderState.height)), d };
 }
 void pathTrace(const Scene &scene, FS::RenderState &renderState) {
-    constexpr float d = 1.f;
-    //TODO: Randomize direction and use multiple samples (IMPORTANT)
-    const Vector origin = { 0, 0, 0 };
-    const Vector2 viewportSize = { 1.f, 1.f };
+    constexpr int SAMPLE_COUNT = 10;
+    constexpr int BOUNCE = 50;
 
+    const Vector origin = { 0, 0, 0 };
     for (int y = 0; y < renderState.height; y++) {
         for (int x = 0; x < renderState.width; x++) {
-            Vector direction = { (x - renderState.width / 2.f) * viewportSize.x / float(renderState.width), (y - renderState.height / 2.f) * viewportSize.y / float(renderState.height), d };
-            FS::Colour color = traceRay(origin, direction, 3, scene);
+            // TODO: Randomize direction and use multiple samples
+            Vector fcolor;
+            for (int i = 0; i < SAMPLE_COUNT; i++) {
+                float samplex = x + (unitRandom() - 0.5f);
+                float sampley = y + (unitRandom() - 0.5f);
+                Vector direction = canvasToViewport(samplex - (renderState.width / 2.f), sampley - (renderState.height / 2.f), renderState);
+                Colour color = traceRay(origin, direction, BOUNCE, scene);
+                fcolor.x += (color.R / 255.f);
+                fcolor.y += (color.G / 255.f);
+                fcolor.z += (color.B / 255.f);
+            }
+            fcolor = fcolor / SAMPLE_COUNT;
+            Colour pixelColor = {
+                uint8_t(std::clamp(fcolor.x * 255.f, 0.f, 255.f)),
+                uint8_t(std::clamp(fcolor.y * 255.f, 0.f, 255.f)),
+                uint8_t(std::clamp(fcolor.z * 255.f, 0.f, 255.f))
+            };
             uint32_t index = x + (y * renderState.width);
-            ((uint32_t *)renderState.screenBuffer)[index] = FS::rgbtoHex(color);
+            ((uint32_t *)renderState.screenBuffer)[index] = FS::rgbtoHex(pixelColor);
         }
     }
 }
@@ -159,19 +206,19 @@ int main() {
 
     Scene scene;
     scene.spheres.reserve(32);
-    scene.spheres.emplace_back(Vector{ 1, 0, 3 }, 0.2f, Material{ { 255, 0, 0 } , 0.4f});
-    scene.spheres.emplace_back(Vector{ 0, 0, 3 }, 0.2f, Material{ { 0, 255, 0 } , 0.3f});
-    scene.spheres.emplace_back(Vector{ -1, 0, 3}, 0.2f, Material{ { 0, 0, 255 } , 0.4f });
-    scene.spheres.emplace_back(Vector{ 0, 100.3f, 0 }, 100.f, Material{ { 255, 255, 255 } ,0.4f });
+    scene.spheres.emplace_back(Vector{ -0.5f, 0, 1 }, 0.2f, Material{ { 255, 0, 0 }, 0.4f });
+    scene.spheres.emplace_back(Vector{ 0, 0, 1 }, 0.2f, Material{ { 0, 255, 0 }, 0.3f });
+    scene.spheres.emplace_back(Vector{ 0.5f, 0, 1 }, 0.2f, Material{ { 0, 0, 255 }, 0.4f });
+    scene.spheres.emplace_back(Vector{ 0, 100.2f, 0 }, 100.f, Material{ { 255, 255, 255 }, 0.4f });
 
     scene.lights.reserve(32);
-    scene.lights.emplace_back(Vector{ 0, 0, 0 }, Vector{ 0, 0, 0 }, 0.3f, LightType::LT_AMBIENT);
     scene.lights.emplace_back(Vector{ 0, 0, 0 }, Vector{ -2, 0, 0 }, 0.6f, LightType::LT_DIRECTION);
 
+    pathTrace(scene, renderState);
+    window.swapBuffers();
+    
     while (window.isOpen()) {
-        clearScreen(FS::rgbtoHex(backgroundColor), renderState);
-        pathTrace(scene, renderState);
         window.processMessages();
-        window.swapBuffers();
     }
+
 }
