de2069f18f693a00642e48f231ed1b27c2f1def4
= <shaikh.fahad.77377@gmail.com>
2026-06-17 21:19:14 +0530

    Fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-$CONFIG=Release
-cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE="$CONFIG" -DCMiAKE_EXPORT_COMPILE_COMMANDS=ON
+CONFIG=Release
+cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE="$CONFIG" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build --config "$CONFIG" --parallel
 build/PathTracer
